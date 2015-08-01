e25596b92b28081a080cb0d86075398805cc6e6f
ianb1957 <ianb1957>
2015-08-01 10:15:51 +0000

    Add AsFloatF(), compiler sugar!

--- src/audio/avs-soundtouch.cpp
+++ src/audio/avs-soundtouch.cpp
@@ -180,9 +180,9 @@ AVSValue __cdecl AVSsoundtouch::Create(AVSValue args, void*, IScriptEnvironment*
 	try {	// HIDE DAMN SEH COMPILER BUG!!!
   return new AVSsoundtouch(
       args[0].AsClip(),
-      args[1].AsFloat(100.0), 
-      args[2].AsFloat(100.0), 
-      args[3].AsFloat(100.0), 
+      args[1].AsFloatf(100.0f),
+      args[2].AsFloatf(100.0f),
+      args[3].AsFloatf(100.0f),
       &args[4],
       env);
 
