04c011495a0271740f3c83442609696a876d5f21
ianb1957 <ianb1957>
2016-01-17 06:53:40 +0000

    BaseClasses project to Avisynth solution.

---
+++ src/plugins/DirectShowSource/BaseClasses/streams.cpp
@@ -0,0 +1,7 @@
+//------------------------------------------------------------------------------
+// File: Streams.cpp
+//
+// Desc: DirectShow base classes - defines overall streams architecture. Precompiled headers.
+//------------------------------------------------------------------------------
+
+#include <streams.h>        // DirectShow base class definitions
