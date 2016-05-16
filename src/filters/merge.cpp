51599ee94cc112be178d6d198fb7452cfe1eb55d
ianb1957 <ianb1957>
2016-05-16 08:13:06 +0000

    Fix MergeLuma allow any Planar test(Wilbert).

--- src/filters/merge.cpp
+++ src/filters/merge.cpp
@@ -192,7 +192,7 @@ MergeLuma::MergeLuma(PClip _child, PClip _clip, float _weight, int _test, IScrip
     env->ThrowError("MergeLuma: YUV data only (no RGB); use ConvertToYUY2 or ConvertToYV12");
 
   if (!vi.IsSameColorspace(vi2)) {  // Since this is luma we allow all planar formats to be merged.
-    if (vi.IsPlanar() == vi2.IsPlanar()) {
+    if (!(vi.IsPlanar() && vi2.IsPlanar())) {
       env->ThrowError("MergeLuma: YUV data is not same type. YUY2 and planar images doesn't mix.");
     }
   }
